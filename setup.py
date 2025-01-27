06cf8596053e9ad857bcee5916c39f1e9d8384f2
Tom Gray <tom_gray@blowhard.org>
2025-01-26 19:44:26 -0800

    Fix lint error: comparison to None

--- setup.py
+++ setup.py
@@ -465,7 +465,7 @@ class pil_build_ext(build_ext):
         include_dirs: list[str] = []
 
         pkg_config = None
-        if shutil.which(os.environ.get("PKG_CONFIG", "pkg-config"), os.X_OK) != None:
+        if shutil.which(os.environ.get("PKG_CONFIG", "pkg-config"), os.X_OK) is not None:
             pkg_config = _pkg_config
 
         #
